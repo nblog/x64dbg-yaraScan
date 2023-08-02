e97e8b9956e6ac09eb823317c1dd83814e907eed
r0th3r <503407184@qq.com>
2023-08-02 11:43:40 +0800

    update clone x64dbg sdk

--- x64dbgYaraScan/plugintemplate/clone.cmd
+++ x64dbgYaraScan/plugintemplate/clone.cmd
@@ -3,6 +3,8 @@
 
 cd %~dp0
 
+:: x64dbg version
+set snapshot=snapshot_2023-08-01_14-50
 
 set GIT=https://github.com/x64dbg/PluginTemplate/
 
@@ -11,12 +13,11 @@ setlocal enabledelayedexpansion
 set PLUGIN[0]=plugin
 set PLUGIN[1]=pluginmain
 
-
 :: plugintemplate
 for /l %%i in (0, 1, 1) do ( curl -L %GIT%blob/main/src/!PLUGIN[%%i]!.h?raw=true -o !PLUGIN[%%i]!.h && curl -L %GIT%blob/main/src/!PLUGIN[%%i]!.cpp?raw=true -o !PLUGIN[%%i]!.cpp )
 
 
 :: pluginsdk
-curl https://github.com/x64dbg/x64dbg/releases/download/snapshot/snapshot_2023-04-15_16-57.zip -o x64dbg.zip
+curl -L https://github.com/x64dbg/x64dbg/releases/download/snapshot/%snapshot%.zip -o x64dbg.zip
 tar -xf x64dbg.zip pluginsdk/
 del /Q x64dbg.zip
